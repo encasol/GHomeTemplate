a0f9ea9712e24b26c2b5755ebaf780eee806a6fa
Enric Casals <enric.casals@hp.com>
2021-01-27 20:29:21 +0100

    fake auth

--- src/app.ts
+++ src/app.ts
@@ -2,104 +2,27 @@ import express from 'express';
 import { smarthome, Headers, SmartHomeV1ExecuteResponse, SmartHomeV1ExecuteResponseCommands, SmartHomeV1SyncDevices } from 'actions-on-google';
 import { google, homegraph_v1 } from 'googleapis';
 import { OAuth2Client } from 'google-auth-library';
-import util from 'util';
 import bodyParser from 'body-parser';
 import http from 'http';
 import https from 'https';
 import { IDevice } from './devices/device-interface';
 import { Door } from './devices/door';
 import fs from 'fs';
+import {IAuthProvider, AuthProvider} from './auth/auth-provider'
 
 const app = express();
 const port = 3000;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-const auth = new google.auth.GoogleAuth({
-    scopes: ['https://www.googleapis.com/auth/homegraph'],
-});
 const options: homegraph_v1.Options = {
     version: 'v1'
 };
-
 const homegraph = google.homegraph(options);
-
 const USER_ID = '123';
 
-app.get('/login', (request, response) => {
-    console.log('login');
-    if (request.method === 'GET') {
-        // console.log('Requesting login page ' + request.query.responseurl);
-        response.send(`
-        <html>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <body>
-            <form action="/login" method="post">
-            <input type="hidden"
-                name="responseurl" value="${request.query.responseurl}" />
-            <button type="submit" style="font-size:14pt">
-                Link this service to Google
-            </button>
-            </form>
-        </body>
-        </html>
-        `);
-    } else {
-        // Unsupported method
-        response.status(405).send('Method Not Allowed');
-    }
-});
-
-app.post('/login', (request, response) => {
-    console.log('login');
-    if (request.method === 'POST') {
-        // Here, you should validate the user account.
-        // In this sample, we do not do that.
-        const responseurl = decodeURIComponent(request.body.responseurl);
-        // console.log(`Redirect to ${responseurl}`);
-        return response.redirect(responseurl);
-    } else {
-        // Unsupported method
-        response.status(405).send('Method Not Allowed');
-    }
-});
-
-app.get('/fakeauth', (request, response) => {
-    console.log('fakeauth');
-    const responseurl = util.format('%s?code=%s&state=%s',
-        decodeURIComponent(request.query.redirect_uri as string), 'xxxxxx',
-        request.query.state);
-    console.log(`Set redirect as ${responseurl}`);
-    return response.redirect(
-        `/login?responseurl=${encodeURIComponent(responseurl)}`);
-});
-
-app.post('/faketoken', (request, response) => {
-    console.log('/faketoken');
-    const grantType = request.query.grant_type ?
-      request.query.grant_type : request.body.grant_type;
-    const secondsInDay = 86400; // 60 * 60 * 24
-    const HTTP_STATUS_OK = 200;
-    console.log(`Grant type ${grantType}`);
-  
-    let obj;
-    if (grantType === 'authorization_code') {
-        obj = {
-            token_type: 'bearer',
-            access_token: '123access',
-            refresh_token: '123refresh',
-            expires_in: secondsInDay,
-        };
-    } else if (grantType === 'refresh_token') {
-        obj = {
-            token_type: 'bearer',
-            access_token: '123access',
-            expires_in: secondsInDay,
-        };
-    }
-    response.status(HTTP_STATUS_OK)
-        .json(obj);
-});
+const authProv: IAuthProvider = new AuthProvider(app);
+authProv.init();
 
 let door: Door = new Door();
 let devices: Map<string, IDevice> = new Map([
---
+++ src/auth/auth-provider.ts
@@ -0,0 +1,84 @@
+import { Express } from 'express';
+import { google, homegraph_v1 } from 'googleapis';
+import util from 'util';
+import { smarthome, Headers, SmartHomeV1ExecuteResponse, SmartHomeV1ExecuteResponseCommands, SmartHomeV1SyncDevices } from 'actions-on-google';
+import { OAuth2Client } from 'google-auth-library';
+
+export interface IAuthProvider {
+    init(): void;
+}
+
+export class AuthProvider implements AuthProvider {
+    private app: Express;
+
+    constructor(app: Express) {
+        this.app = app;
+    }
+
+    init() {
+        this.app.get('/login', this.loginGET);
+        this.app.post('/login', this.loginPOST);
+        this.app.get('/fakeauth', this.fakeauthGET);
+        this.app.post('/faketoken', this.faketokenPOST);
+    }
+
+    private loginGET(request, response): void {
+        // console.log('Requesting login page ' + request.query.responseurl);
+        response.send(`
+        <html>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <body>
+            <form action="/login" method="post">
+            <input type="hidden"
+                name="responseurl" value="${request.query.responseurl}" />
+            <button type="submit" style="font-size:14pt">
+                Link this service to Google
+            </button>
+            </form>
+        </body>
+        </html>
+        `);  
+    }
+
+    private loginPOST(request, response): void {
+        console.log('login');
+        // Here, you should validate the user account.
+        // In this sample, we do not do that.
+        const responseurl = decodeURIComponent(request.body.responseurl);
+        // console.log(`Redirect to ${responseurl}`);
+        return response.redirect(responseurl);
+    }
+
+    private fakeauthGET(request, response): void {
+        console.log('fakeauth');
+        const responseurl = util.format('%s?code=%s&state=%s',
+            decodeURIComponent(request.query.redirect_uri as string), 'xxxxxx',
+            request.query.state);
+
+        return response.redirect(`/login?responseurl=${encodeURIComponent(responseurl)}`);
+    }
+
+    private faketokenPOST(request, response): void {
+        console.log('/faketoken');
+        const grantType = request.query.grant_type ? request.query.grant_type : request.body.grant_type;
+        const secondsInDay = 86400; // 60 * 60 * 24
+        const HTTP_STATUS_OK = 200;
+      
+        let obj;
+        if (grantType === 'authorization_code') {
+            obj = {
+                token_type: 'bearer',
+                access_token: '123access',
+                refresh_token: '123refresh',
+                expires_in: secondsInDay,
+            };
+        } else if (grantType === 'refresh_token') {
+            obj = {
+                token_type: 'bearer',
+                access_token: '123access',
+                expires_in: secondsInDay,
+            };
+        }
+        response.status(HTTP_STATUS_OK).json(obj);
+    }
+}
